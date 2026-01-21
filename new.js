9a636a1456b5d7d2bca957abb1330ce0df460adf
habibtariq513 <157604419+habibtariq513@users.noreply.github.com>
2026-01-21 22:18:15 +0000

    let Variable

--- new.js
+++ new.js
@@ -1,1 +1,13 @@
-console.log("HI")
+let accountEmail = "habib@gmail.com"
+var pass = "1243"
+const num = 32
+
+console.table([accountEmail, pass, num])
+
+/*
+Prefer not to use var
+because of issue in block & functional Scope    
+ */
+
+
+
