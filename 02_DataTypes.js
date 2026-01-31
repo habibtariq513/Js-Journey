771d3c95636bebc10171a8339fc7fa5068bd69a3
habibtariq513 <157604419+habibtariq513@users.noreply.github.com>
2026-01-31 21:45:32 +0000

    Data Types

---
+++ 02_DataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we're using nodejs, not browser
+
+let name = 'Habib'
+let age = 21
+let isLoggedIn = true
+
+// number => 2 to power 53 
+// BigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined 
+// Symbol => unique
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
+
+
+
+
