5a8903df8e7239bb2e881550e6bd00b49377ee90
habibtariq513 <157604419+habibtariq513@users.noreply.github.com>
2026-02-01 22:29:16 +0000

    Conversion of Data Types

---
+++ 03_ConversionOperation.js
@@ -0,0 +1,26 @@
+let score = "33"
+
+console.log(typeof score);
+
+let ValueInNumber = Number(score)
+console.log(typeof ValueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1, false => 0
+
+let isLoggedIn = ""
+let boolisLoggedIn = Boolean(isLoggedIn)
+console.log(boolisLoggedIn);
+
+// 1 => true, 0 => false
+// "" => false
+// "hitesh" => true
+
+
+let someNumber = 33
+
+let strNumber = String(someNumber)
+console.log(strNumber);
+
+ 
