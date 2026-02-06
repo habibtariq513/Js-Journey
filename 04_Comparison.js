f69b7697cc3f059249e77ba3f773ee426ef967ff
habibtariq513 <157604419+habibtariq513@users.noreply.github.com>
2026-02-06 22:38:41 +0000

    04_Comparison

---
+++ 04_Comparison.js
@@ -0,0 +1,15 @@
+console.log("2" > 1);
+
+console.log("Null:");
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log("Undefined:");
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+// === --> Ye strictly chk krta hai Comparison ma Data Types same hai ya nai 
+console.log("\nStrict Checkup: ");
+console.log("2" === 1);
