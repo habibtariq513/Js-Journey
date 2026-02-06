3af966bfc885286973fe723f6a2e4b04a702e409
habibtariq513 <157604419+habibtariq513@users.noreply.github.com>
2026-02-06 21:28:08 +0000

    03_ConversionOperation

--- 03_ConversionOperation.js
+++ 03_ConversionOperation.js
@@ -24,12 +24,25 @@ let strNumber = String(someNumber)
 console.log(strNumber);
 
  // ================= Operations =================
+
+ console.log("\nStart Here: "); 
  console.log("1" + 2);
  console.log(1 + "2");
  console.log(1 + "2" + "2");
  console.log("1" + 2 + 2);
- console.log(1 + 2 + 2);
+ console.log(1 + 2 + "2");
+ console.log(1 + 2 + 5 + "2" + "3"); // if string value is starting after Integer then all values will be considered as a string and the values before string will be considered as an integer
  
+console.log(`a:${someNumber}`);
+
+let num1, num2, num3
+num1 = num2 = num3 = 1 + 1
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
+
+
 
 
 
