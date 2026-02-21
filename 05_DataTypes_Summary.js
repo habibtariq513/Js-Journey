f903b4c253f33e2c03c148693883b99d1f1e6901
habibtariq513 <157604419+habibtariq513@users.noreply.github.com>
2026-02-21 21:10:45 +0000

    05_DataTypes_Summary

---
+++ 05_DataTypes_Summary.js
@@ -0,0 +1,40 @@
+// ---- Primitive Data Types ---- 
+
+// 7 Types: String, Integer, Boolean, Null, Undefined, Symbol, BigInt(Some scientific Values than can't handle by Int datatype)
+
+// Undefined: Memory space declared hai or Variable declared hai lekin abhi usma value nahi daali is ko Undefined kehtay hain
+// Symbol: Kisi bhi value ko unique bnanay k liye SYMBOL use hota hai
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail; // Undefined
+// let userEmail = undefined; another way of Undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 4787888948957835835897458468n
+
+// ---- Reference Data Types (Non-Primitive Data Types) ---- 
+// Arrays, Functions, Objects
+
+const heros = ["shkatiman", "Doga", "Nagraaj"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
