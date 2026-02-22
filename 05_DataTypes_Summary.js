d17f8494256e038eb63abb6035aac7882e71f142
habibtariq513 <157604419+habibtariq513@users.noreply.github.com>
2026-02-22 09:16:08 +0000

    05_DataTypes_Summary

--- 05_DataTypes_Summary.js
+++ 05_DataTypes_Summary.js
@@ -23,8 +23,41 @@ const bigNumber = 4787888948957835835897458468n
 // ---- Reference Data Types (Non-Primitive Data Types) ---- 
 // Arrays, Functions, Objects
 
+// Arrays
 const heros = ["shkatiman", "Doga", "Nagraaj"]
 
+// Object
+let my_Obj = {
+    name: "Habib",
+    age: 21
+}
+
+// Function
+const MyFunction = function(){
+    console.log("Hello World");  
+}
+
+// ---- Typeof function for Non-Primitive Types ---- 
+
+// --> Non-Primitive types will always be returned as an OBJECT type by "typeof function".
+// --> But the type of function will be called "Object function"
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+console.log("Arrays: ", typeof heros);
+console.log("Object: ", typeof my_Obj);
+console.log("Function: ", typeof MyFunction);
+
+// ---- Typeof function for Primitive Types ---- 
+// NULL will be returned as as OBJECT
+
+console.log("\nInteger: ", typeof score);
+console.log("Float: ", typeof scoreValue);
+console.log("Boolean: ", typeof isLoggedIn);
+console.log("Null: ", typeof outsideTemp);
+console.log("Undefined: ", typeof userEmail);
+console.log("Symbol: ", typeof id);
+console.log("BigInt: ", typeof bigNumber);
 
 
 
