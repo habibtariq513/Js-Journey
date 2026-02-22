efcac698d677294a9cd888e2e57ce0626733ac23
habibtariq513 <157604419+habibtariq513@users.noreply.github.com>
2026-02-22 12:45:07 +0000

    06_Strings

---
+++ 06_Strings.js
@@ -0,0 +1,8 @@
+const name = "Habib"
+const repoCount = 10
+
+// console.log(name + repoCount + "Value");
+
+// String Interpolation
+console.log(`My name is ${name} and I've total ${repoCount} repos on GitHub.`);
+
