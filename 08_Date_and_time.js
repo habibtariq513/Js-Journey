51d6f94a66b689a91a5f382f7ea474148af3af90
habibtariq513 <157604419+habibtariq513@users.noreply.github.com>
2026-02-25 15:31:14 +0000

    08_Date_and_time

--- 08_Date_and_time.js
+++ 08_Date_and_time.js
@@ -12,15 +12,27 @@ console.log(`Date wtih toDateString(): ${MyDate.toDateString()}`);
 
 console.log(`\nType of date: ${typeof MyDate}`);
 
-// Hard Coded Date
+// Hard Coded Date 
 let myCreatedDate = new Date(2026, 1, 25, 5, 55) // Year, Month(Starts from Zero), Date, hour, minute
 console.log(`myCreatedDate: ${myCreatedDate.toDateString()}`);
 console.log(`myCreatedDate with Time : ${myCreatedDate.toLocaleString()}`);
 
+// for YY-MM-DD format
+// let NewCreatedDate = new Date("2025-02-25")
+let NewCreatedDate = new Date("02-25-2025") // MM-DD-YY format
+console.log(`NewCreatedDate: ${NewCreatedDate.toLocaleString()}`);
+
+// Time Stamps
+let timeStamps = Date.now()
+console.log(`Time Stamps: ${timeStamps}`);
+
+console.log(`myCreatedDate with getTime(): ${myCreatedDate.getTime()}`);
+console.log(`myCreatedDate with getTime(): ${Math.floor((Date.now())/1000)}`);
 
 
 
 
 
 
+ 
  
