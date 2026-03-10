441f37e97235148eecf8dcdb65fd0aa85408fef6
habibtariq513 <157604419+habibtariq513@users.noreply.github.com>
2026-03-10 12:30:12 +0000

    01_Arrays

---
+++ 02_Basics/01_Arrays.js
@@ -0,0 +1,61 @@
+const myArr = [1, 2, 3, 4, 5]
+const MyHeros = ["Shakitiman", "Naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+console.log(myArr[1]);
+
+// ====== Array Methods ======
+
+// Push Method
+myArr.push(5)
+console.log(`Push Method: ${myArr}`)
+
+// Pop Method
+myArr.pop()
+console.log(`Pop Method: ${myArr}`)
+
+// Unshift Method
+myArr.unshift(90)
+console.log(`Unshift Method: ${myArr}`)
+
+// Shift Method
+myArr.shift()
+console.log(`Shift Method: ${myArr}`)
+
+// Include Method
+console.log(`Includes Method: ${myArr.includes(3)}`);
+
+// Indexof Method
+console.log(`Indexof Method: ${myArr.indexOf(3)}`);
+
+// Join Method
+const newArr = myArr.join()
+console.log(`Join Method: ${myArr}`);
+console.log(`Join Method: ${newArr}`);
+console.log(`Join Method: ${typeof newArr}`);
+
+// Slice Method
+// -> It doesn't affect over Actual Array
+const newArr2 = myArr.slice(1, 4)
+console.log(`\nSlice Method: ${newArr2}`);
+console.log(`Slice Method: ${myArr}`);  // 1,2,3,4,5
+
+// Splice Method
+// -> It affects over Actual Array
+const newArr3 = myArr.splice(1, 3)
+console.log(`\nSplice Method: ${newArr3}`);
+console.log(`Splice Method: ${myArr}`);  // 1,5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
