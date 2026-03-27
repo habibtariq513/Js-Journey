ec71908ae8361bdfde0081118abea13c635484bb
habibtariq513 <157604419+habibtariq513@users.noreply.github.com>
2026-03-27 22:05:18 +0000

    01_Arrays

--- 02_Basics/01_Arrays.js
+++ 02_Basics/01_Arrays.js
@@ -68,6 +68,10 @@ const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
 const real_another_array = another_array.flat(Infinity)
 console.log(real_another_array);
 
+console.log(Array.isArray("Hitesh"));
+console.log(Array.from("Hitesh"));
+console.log(Array.from({name: "Hitesh"})); //interesting
+
 
 
 
