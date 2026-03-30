0537ddebfb62cce1daaf5f61223dc73b46b31243
habibtariq513 <157604419+habibtariq513@users.noreply.github.com>
2026-03-30 21:53:11 +0000

    02_Objects

---
+++ 02_Basics/02_Objects.js
@@ -0,0 +1,21 @@
+// Symbols
+const mySym = Symbol("Key1")
+
+// Object Literals
+
+const JsUser = {
+    name: "Habib",
+    [mySym]: "MyKey1",  // usage of Symbol in Object  [Interview Question]
+    age: 21,
+    location: "Lahore",
+    email: "habib@devsinc.com",
+    isLoggedIn: false,
+    lastLoggedIn: ["Monday", "Tuesday"]
+}
+
+console.log("JsUser:", JsUser["email"]);
+console.log("Symbol and Object:", JsUser[mySym]);
+
+
+
+
