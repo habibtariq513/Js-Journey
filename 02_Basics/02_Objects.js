26de62994e45a7528032d0a93d70d161048255b7
habibtariq513 <157604419+habibtariq513@users.noreply.github.com>
2026-04-18 21:29:31 +0000

    02_Objects

--- 02_Basics/02_Objects.js
+++ 02_Basics/02_Objects.js
@@ -71,10 +71,14 @@ console.log("First Name:", regularUser.fullname.username.firstname);
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
 
  // Array type merging of two Objects
 // const obj3 = {obj1, obj2}
- console.log(obj3);
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+const obj3 = {...obj1, ...obj2}
+console.log(obj3);
+
 
 
  
