f7b95dccb0e2801e4688e5af5acbe45d4929b63d
habibtariq513 <157604419+habibtariq513@users.noreply.github.com>
2026-05-22 14:26:06 +0000

    01_Functions.js

--- 03_Basics/01_Functions.js
+++ 03_Basics/01_Functions.js
@@ -8,11 +8,20 @@ function sayMyName() {
 
 // sayMyName()
 
+// function addTwoNum(num1, num2) {
+//     console.log(`Sum: ${num1 + num2}`);    
+// }
+
+// addTwoNum(4, "6")
+// addTwoNum(4, null)
+// addTwoNum(4, "a")
+// addTwoNum(4, 5)
+
 function addTwoNum(num1, num2) {
-    console.log(`Sum: ${num1 + num2}`);    
+    return num1 + num2    
 }
 
-addTwoNum(4, "6")
-addTwoNum(4, null)
-addTwoNum(4, "a")
-addTwoNum(4, 5)
+const result = addTwoNum(4, 5)
+console.log(`Result: ${result}`);
+
+
