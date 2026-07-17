64e6861d3b9dfbc386bf1de4ae1baba02cfdac36
habibtariq513 <157604419+habibtariq513@users.noreply.github.com>
2026-07-17 05:16:33 +0000

    02_Scopes

---
+++ 03_Basics/02_Scopes.js
@@ -0,0 +1,11 @@
+function one() {
+    const username = "Habib"
+    function two() {
+        const website = "Youtube"
+        console.log(username);        
+    }
+    console.log(website);
+    two()    
+}
+
+one()
