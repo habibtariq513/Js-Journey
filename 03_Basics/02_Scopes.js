0e5f1c97f423ba6ec9c7a978e513db0faa261409
habibtariq513 <157604419+habibtariq513@users.noreply.github.com>
2026-07-17 11:49:45 +0000

    02_Scopes

--- 03_Basics/02_Scopes.js
+++ 03_Basics/02_Scopes.js
@@ -17,6 +17,7 @@ function addOne(num) {
 
 addOne(4)
 
+// Here "addTwo" is often called an Expression which is storing Function. As variables in JS has the capability to store Functions, value and JSON Values, etc.
 const addTwo = function(num) {
     return num + 2
 }
