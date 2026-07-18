f90f60ede47fc5f554d25e48afacdbd137821b8f
habibtariq513 <157604419+habibtariq513@users.noreply.github.com>
2026-07-18 18:01:21 +0000

    01_Loops

---
+++ 04_Iterations/01_Loops.js
@@ -0,0 +1,13 @@
+// for of Loop
+const arr = [11, 22, 33, 44]
+for (const i of arr) {
+    // console.log(i);    
+}
+
+const greet = "Hello Jani"
+for (const i of greet) {
+    console.log(i);
+    
+}
+
+
