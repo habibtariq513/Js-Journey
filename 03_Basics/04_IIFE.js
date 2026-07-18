1cf5ca8fafb1b1da518eb930890f61c09b704064
habibtariq513 <157604419+habibtariq513@users.noreply.github.com>
2026-07-18 07:55:08 +0000

    04_IIFE

--- 03_Basics/04_IIFE.js
+++ 03_Basics/04_IIFE.js
@@ -1,2 +1,13 @@
 // IIFE --> Immediately Invoked Function Expressions
- 
+// We use it when we don't want our specific function to be polluted due to Global Scope
+
+// Common Way
+function chai() {
+    console.log(`Common Way: DB CONNECTED.`);    
+}
+chai();
+
+// Withh IIFE Technique
+(function iifeTech() {
+    console.log(`IIFE Tchnique: DB CONNECTED.`);
+})()
