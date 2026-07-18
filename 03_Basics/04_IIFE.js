d2f5c8504c6b915504291d92be20e0b87e75c849
habibtariq513 <157604419+habibtariq513@users.noreply.github.com>
2026-07-18 09:35:21 +0000

    04_IIFE.js

--- 03_Basics/04_IIFE.js
+++ 03_Basics/04_IIFE.js
@@ -1,5 +1,5 @@
 // IIFE --> Immediately Invoked Function Expressions
-// We use it when we don't want our specific function to be polluted due to Global Scope
+// We use it when we don't want our specific function to be polluted due to Global Scope declarations.
 
 // Common Way
 function chai() {
@@ -8,6 +8,20 @@ function chai() {
 chai();
 
 // Withh IIFE Technique
+// SYNTAX: (Function Definition Paranthesis)(Execution Paranthesis)
 (function iifeTech() {
+    // Named IIFE:
     console.log(`IIFE Tchnique: DB CONNECTED.`);
-})()
+})();
+
+// Using Arrow
+(() => {
+    // Unamed IIFE:
+    console.log(`Arrow Tchnique: DB CONNECTED.`);
+})();
+
+// Parameters passing in Unamed IIFE
+((name) => {
+    console.log(`Arrow Tchnique: DB CONNECTED to ${name}.csv.`);
+})('Habib');
+
