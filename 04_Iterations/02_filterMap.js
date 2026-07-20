646e8837ad1d62681fe318bed3abbdf56c126bfb
habibtariq513 <157604419+habibtariq513@users.noreply.github.com>
2026-07-20 15:00:31 +0000

    02_filterMap

--- 04_Iterations/02_filterMap.js
+++ 04_Iterations/02_filterMap.js
@@ -1,8 +1,14 @@
-const coding = ["js", "cpp", "java", "python"]
+// const coding = ["js", "cpp", "java", "python"]
 
-const values = coding.forEach((item) => {
-    // console.log(item);    
-    return item;
-})
+// const values = coding.forEach((item) => {
+//     // console.log(item);    
+//     return item;
+// })
 
-// console.log(values); // undefined
+// // console.log(values); // undefined
+
+// -------------- Filters --------------
+const numArr = [1,2,3,4,5,6,7]
+console.log(numArr.filter(
+    (num) => num > 5
+));
