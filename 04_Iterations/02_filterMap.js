0016af45af8a428af7e0e0f0d87cd6286686eb0e
habibtariq513 <157604419+habibtariq513@users.noreply.github.com>
2026-07-20 14:17:05 +0000

    02_filterMap

---
+++ 04_Iterations/02_filterMap.js
@@ -0,0 +1,8 @@
+const coding = ["js", "cpp", "java", "python"]
+
+const values = coding.forEach((item) => {
+    // console.log(item);    
+    return item;
+})
+
+// console.log(values); // undefined
