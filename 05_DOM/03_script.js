f2660d264138465355f38c609a0301d02d280b39
habibtariq513 <157604419+habibtariq513@users.noreply.github.com>
2026-07-24 11:53:36 +0000

    Refactor DOM structure and update JavaScript interactions

--- 05_DOM/03_script.js
+++ 05_DOM/03_script.js
@@ -1,1 +1,23 @@
-alert("HeeeeeeeeeeeeHahaaaaaa")
+// console.log(document.querySelector("div").textContent);
+// console.log(document.querySelector("div").innerHTML);
+// console.log(document.querySelector("div").innerText);
+
+// let h3 = document.querySelector("h3")
+// h3.innerText = h3.innerText + " Vegetables"
+// console.log(h3.innerText)
+
+
+let boxes = document.querySelectorAll(".box")[0]
+boxes.innerText = boxes.innerText + " Bhai"
+console.log(boxes.innerText);
+
+let boxes_2 = document.querySelectorAll(".box")[1]
+boxes_2.innerText = boxes_2.innerText + " Yousafi"
+console.log(boxes_2.innerText);
+
+let boxes_3 = document.querySelectorAll(".box")[2]
+boxes_3.innerText = boxes_3.innerText + " Yousafi"
+console.log(boxes_3.innerText);
+
+
+
