dccc20f5ce9f99f2b5bbb36681ab8fb1689ad0d0
habibtariq513 <157604419+habibtariq513@users.noreply.github.com>
2026-07-24 14:11:44 +0000

    Update HTML structure and enhance JavaScript comments for clarity

--- 05_DOM/03_script.js
+++ 05_DOM/03_script.js
@@ -1,23 +1,33 @@
-// console.log(document.querySelector("div").textContent);
-// console.log(document.querySelector("div").innerHTML);
-// console.log(document.querySelector("div").innerText);
+// QuerySelector: Returns the fist element 
+// QuerySelectorAll: Returns the Node List
+
+// console.log(document.querySelector("div").textContent); // Returns texual Content even for hidden elements
+// console.log(document.querySelector("div").innerHTML); // Returns text along with tags enclosed
+// console.log(document.querySelector("div").innerText); // returns the text written in tags only
 
 // let h3 = document.querySelector("h3")
 // h3.innerText = h3.innerText + " Vegetables"
 // console.log(h3.innerText)
 
 
-let boxes = document.querySelectorAll(".box")[0]
-boxes.innerText = boxes.innerText + " Bhai"
-console.log(boxes.innerText);
+// let boxes = document.querySelectorAll(".box")[0]
+// boxes.innerText = boxes.innerText + " Bhai"
+// console.log(boxes.innerText);
 
-let boxes_2 = document.querySelectorAll(".box")[1]
-boxes_2.innerText = boxes_2.innerText + " Yousafi"
-console.log(boxes_2.innerText);
+// let boxes_2 = document.querySelectorAll(".box")[1]
+// boxes_2.innerText = boxes_2.innerText + " Yousafi"
+// console.log(boxes_2.innerText);
 
-let boxes_3 = document.querySelectorAll(".box")[2]
-boxes_3.innerText = boxes_3.innerText + " Yousafi"
-console.log(boxes_3.innerText);
+// let boxes_3 = document.querySelectorAll(".box")[2]
+// boxes_3.innerText = boxes_3.innerText + " Yousafi"
+// console.log(boxes_3.innerText);
 
+// -------- getAttrubute() --------
+// Used to get 
+console.log(document.querySelector("div").getAttribute("id"))
 
+// -------- setAttrubute() --------
+console.log(document.querySelectorAll("div")[1].setAttribute("id", "newBox"))
 
+// -------- node.style --------
+console.log(document.querySelectorAll("div")[0].style.backgroundColor = "brown")
